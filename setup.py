e3d04828a1097471d0e1065aa22c2ff5eede7771
Ashish Shevale <shevaleashish@gmail.com>
2020-12-17 10:53:41 +0530

    Updated setup.py file to build rle_fast extension

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 import setuptools
 
+rle_fast_extension = setuptools.Extension("rle.rle_fast",
+      sources = ["rle_fast/rle_fast_extension.c"] )
+
 with open('README.md', "r") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='python-rle', 
+    name='python-rle',
     version="0.0.3",
     author="Tan Nian Wei",
     author_email="tannianwei@aggienetwork.com",
     description="Run-length encoding for data analysis in Python",
+    ext_modules = [rle_fast_extension],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tnwei/python-rle",
